5c291e6793c08a49b0ef47c409621efb2e068003
Tamnil Saito Junior <tamnil@gmail.com>
2019-06-12 19:03:30 -0300

    new commands added - swarm

--- zsh-docker-abbreviation.plugin.zsh
+++ zsh-docker-abbreviation.plugin.zsh
@@ -18,6 +18,16 @@ alias dke='docker exec'
 alias dkeit='docker exec -it '
 alias dkr='docker run'
 alias dkrit='docker run -it '
+alias dkpu='docker push'
+alias dkpl='docker pull'
+alias dklog='docker log'
+alias dkstop='docker stop'
+alias dkstart='docker start'
+alias dke='docker events'
+alias dkh="docker history"
+alias dkinfo="docker info"
+alias dkkill="docker kill"
+
 
 # System cleaning
 alias dkspa='docker system prune -a '
@@ -25,3 +35,10 @@ alias dkrm='docker rm '
 alias dkrma='docker rm $(docker ps -a -q)'
 alias dkrmi='docker rmi '
 alias dkrmia='docker rmi $(docker images -q)'
+
+alias dksw='docker swarm'
+alias dkswj='docker swarm join'
+alias dkswi='docker swarm init'
+alias dkswl='docker swarm leave'
+alias dkswupd='docker swarm update'
+
